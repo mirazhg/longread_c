b029fb15328f00df7f980adc7df92f47d0e32697
Mirazh <mirazhg@gmail.com>
2015-04-10 12:32:15 +0100

    d

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -5,6 +5,18 @@ class FieldsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def update
+    @article = Article.find(params[:id])
+    redirect_to article_path(@article) if @article.fields.update(field_params)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @field = @article.fields.find(params[:id])
+    @field.destroy
+    redirect_to article_path(@article)
+  end
+
   private
     def field_params
       params.require(:field).permit( :caption,:caver_pict,:lead,:body,:teg,:unicum)
