ff9e50c7470f4d32655780d5fc1437dc471aaace
Mirazh <mirazhg@gmail.com>
2015-04-14 07:47:38 +0100

    longread  ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(window).on("scroll", function() {
+    $('.l-field_caver').each(function() {
+        if (($(window).scrollTop() >= $(this).offset().top)&&($(window).scrollTop() < ($(this).next().offset().top))) {
+            if (!($(this).children('.b-field_caver').hasClass('bg_article_none'))) {
+                $('.bg_article').css('background-image', $(this).children('.b-field_caver').css('background-image'));
+                $(this).children('.b-field_caver').addClass('bg_article_none');
+            }
+        }
+        else {
+            if ($(this).children('.b-field_caver').hasClass('bg_article_none')) { $(this).children('.b-field_caver').removeClass('bg_article_none');}
+        }
+
+        //end;
+    });
+});
