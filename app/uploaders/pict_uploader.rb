fe930619dd32de8b00ba488e2a88aa4bc767dd0d
Mirazh <mirazhg@gmail.com>
2015-04-17 12:48:32 +0100

    cloudinany setting

--- app/uploaders/pict_uploader.rb
+++ app/uploaders/pict_uploader.rb
@@ -20,6 +20,8 @@ class PictUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  if Rails.env.production? then cloudinary_transformation :image_metadata=>true
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
