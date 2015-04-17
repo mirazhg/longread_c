7d3f30bd924affae01065de87f4f734b3783d39f
Mirazh <mirazhg@gmail.com>
2015-04-17 15:38:09 +0100

    edit card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,11 +5,11 @@ class CardsController < ApplicationController
     @cards = @field.cards.all
 
   end
-  # def edit
-  #   @article = Article.find(params[:article_id])
-  #   @field = @article.fields.find(params[:field_id])
-  #   @card = @article.fields.cards.find(params[:id])
-  # end
+  def edit
+    @article = Article.find(params[:article_id])
+    @field = @article.fields.find(params[:field_id])
+    @card = @field.cards.find(params[:id])
+  end
 
   def create
     @article = Article.find(params[:article_id])
@@ -19,17 +19,17 @@ class CardsController < ApplicationController
     redirect_to (:back)
   end
 
-  # def update
-  #   @article = Article.find(params[:article_id])
-  #   @field = @article.fields.find(params[:field_id])
-  #   @card = @article.fields.cards.find(params[:id])
-  #   if @card.update(field_params)
-  #     redirect_to article_path(@article)
-  #   else
-  #     render 'edit'
-  #   end
-  #
-  # end
+  def update
+    @article = Article.find(params[:article_id])
+    @field = @article.fields.find(params[:field_id])
+    @card = @field.cards.find(params[:id])
+    if @card.update(card_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
 
   def destroy
     @article = Article.find(params[:article_id])
