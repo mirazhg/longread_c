c2933f3ffc9c65786c3dc187375b9353b00a80a7
Mirazh <mirazhg@gmail.com>
2015-04-21 14:34:33 +0100

    cancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :article
-  has_many :users_roles
-  has_many :roles, :through => :users_roles
-
 
 end
